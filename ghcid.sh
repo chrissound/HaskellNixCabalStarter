26b6fc627d2d3e41ffaea74909fd7bdb16eb19f1
Chris S <chris@soundshare.co.uk>
2019-12-14 12:48:12 +0000

    fix command

--- ghcid.sh
+++ ghcid.sh
@@ -1,7 +1,6 @@
+#!/usr/bin/env bash
 if which nix; then
-    local cmd_escaped
-    printf -v cmd_escaped "%q" "ghcid --command='cabal v2-repl app' --test=Main.main"
-    nix-shell --run "$cmd_escaped"
+    nix-shell --run "ghcid --command='cabal v2-repl app' --test=Main.main"
     exit 0
 elif which stack; then
     ghcid '--command=stack ghci' --test='Main.main'
