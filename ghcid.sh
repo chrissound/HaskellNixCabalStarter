b93e2ef0587a78640f1948e025aaadeb0a3ce266
Chris S <chris@soundshare.co.uk>
2019-12-14 12:40:53 +0000

    update ghcid for running in nix shell, addition of hpack package.yaml

--- ghcid.sh
+++ ghcid.sh
@@ -1,4 +1,9 @@
 if which nix; then
-    ghcid --command="cabal v2-repl app" --test=Main.main
+    local cmd_escaped
+    printf -v cmd_escaped "%q" "ghcid --command='cabal v2-repl app' --test=Main.main"
+    nix-shell --run "$cmd_escaped"
+    exit 0
+elif which stack; then
+    ghcid '--command=stack ghci' --test='Main.main'
     exit 0
 fi
